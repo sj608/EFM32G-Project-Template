1f1325275849460c2396d780975ac721fd1dc53c
SJ park <sjpark@SJs-MacBook-Air.local>
2024-12-01 17:30:14 -0800

    added FreeRTOS folder. Need to re-configure the Cmake to properly build the project

---
+++ FreeRTOS/portable/CMakeFiles/freertos_kernel_port.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/projects/efm-lib/FreeRTOS/portable/GCC/ARM_CM3/port.c" "/home/pi/projects/efm-lib/FreeRTOS/portable/CMakeFiles/freertos_kernel_port.dir/GCC/ARM_CM3/port.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "portable/GCC/ARM_CM3"
+  "include"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/projects/efm-lib/FreeRTOS/CMakeFiles/freertos_kernel.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FreeRTOS/portable/CMakeFiles/freertos_kernel_port.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/freertos_kernel_port.dir/GCC/ARM_CM3/port.c.o"
+  "libfreertos_kernel_port.a"
+  "libfreertos_kernel_port.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/freertos_kernel_port.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
